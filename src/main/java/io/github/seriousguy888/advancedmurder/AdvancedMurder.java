716340b97ce0eaea1f81e6ff0a0c4c068862c128
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-11-10 11:50:37 -0500

    Initial commit

---
+++ src/main/java/io/github/seriousguy888/advancedmurder/AdvancedMurder.java
@@ -0,0 +1,17 @@
+package io.github.seriousguy888.advancedmurder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedMurder extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
